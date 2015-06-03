315b4c9e8a84201aa5d0e58385a4982aaf4327e0
henrymatt <henrymatt@me.com>
2015-06-04 08:37:08 +0900

    Persists logged in user

--- public/app/account/vtmsIdentity.js
+++ public/app/account/vtmsIdentity.js
@@ -1,6 +1,10 @@
-angular.module('vtms').factory('vtmsIdentity', function() {
+angular.module('vtms').factory('vtmsIdentity', function($window) {
+  var currentUser;
+  if(!!$window.bootstrappedUserObject) {
+    currentUser = $window.bootstrappedUserObject;
+  }
   return {
-    currentUser: undefined,
+    currentUser: currentUser,
     isAuthenticated: function() {
       return !!this.currentUser;
     }
