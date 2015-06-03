9e77c856a7491ef164413e5157868be7c828df1c
henrymatt <henrymatt@me.com>
2015-06-04 08:48:40 +0900

    Refactors server.js

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-var express = require('express'),
-    models = require('./server/models/models.js'),
-    passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
+var express = require('express');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -13,33 +10,7 @@ require('./server/config/express')(app, config);
 
 require('./server/config/sequelize');
     
-passport.serializeUser(function(user, done) {
-  if(user) {
-    done(null, user.id);
-  }
-});
-
-passport.deserializeUser(function(id, done) {
-  models.User.find({where: {id: id}})
-    .then(function(user) {
-      done(null, user);
-    })
-    .error(function(err) {
-      done(err, null);
-  });
-});
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    models.User.find({ where: {username: username}}).then(function(user) {
-      if(user && user.authenticate(password)) {
-        return done(null, user);
-      } else {
-        return done(null, false);
-      }
-    })
-  }
-));
+require('./server/config/passport')();
 
 require('./server/config/routes')(app);
 
