456fc362c7092d03a04f4141e697bb22a56c6b1c
henrymatt <henrymatt@me.com>
2015-06-04 08:11:16 +0900

    Removes unused modules and implements hashed passwords

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
 var db = require('../config/sequelize.js'),
-    Sequelize = require('sequelize');
+    Sequelize = require('sequelize'),
+    crypto = require('crypto');
   
 var User = db.define('user', {
   id: {
@@ -17,13 +18,42 @@ var User = db.define('user', {
   },
   username: {
     type: Sequelize.STRING
+  },
+  salt: {
+    type: Sequelize.STRING
+  },
+  hashed_pwd: {
+    type: Sequelize.STRING
   }
 }, {
-  timestamps: true
+  timestamps: true,
+  instanceMethods: {
+    authenticate: function(passwordToMatch) {
+      return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+    }
+  }
+  
 });
 
 User.sync({force: true}).then(function() {
-  User.create({firstName: "Matt", lastName: "Henry", username: "henrymatt"});
+  var salt = createSalt();
+  var hash = hashPwd(salt, 'pikachu');
+  User.create({
+    firstName: "Matt", 
+    lastName: "Henry", 
+    username: "henrymatt",
+    salt: salt,
+    hashed_pwd: hash
+  });
 });
+
+function createSalt() {
+  return crypto.randomBytes(128).toString('base64');
+}
+
+function hashPwd(salt, pwd) {
+  var hmac = crypto.createHmac('sha1', salt);
+  return hmac.update(pwd).digest('hex');
+}
 
 module.exports = User;
