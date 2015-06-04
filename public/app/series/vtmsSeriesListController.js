c12a027956cd103187ae61e42791b239a0860fd1
henrymatt <henrymatt@me.com>
2015-06-04 22:17:21 +0900

    Pulls series info from database

--- public/app/series/vtmsSeriesListController.js
+++ public/app/series/vtmsSeriesListController.js
@@ -1,3 +1,7 @@
 angular.module('vtms').controller('vtmsSeriesListController', function($scope, vtmsSeries) {
   $scope.series = vtmsSeries.query();
+  
+  $scope.sortOptions = [{value: "title", text: "Sort by Title"},
+                        {value: "code", text: "Sort by Code"}];
+  $scope.sortOrder = $scope.sortOptions[0].value;
 });
