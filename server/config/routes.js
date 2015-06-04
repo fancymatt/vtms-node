15e121adfb4e3870ee3ef10d0e18a8d6e1b2fe95
henrymatt <henrymatt@me.com>
2015-06-04 09:15:32 +0900

    Supports server-side authentication

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,13 @@
-var auth = require('./auth');
+var auth = require('./auth'),
+    models = require('../models/models');
 
 module.exports = function(app) {
+  
+  app.get('/users', auth.requiresRole('admin'), function(req, res) {
+    models.User.findAll().then(function(users) {
+      res.send(users);
+    });
+  });
   
   app.get('/partials/*', function(req, res) {
     res.render('../../public/app/' + req.params[0]);
