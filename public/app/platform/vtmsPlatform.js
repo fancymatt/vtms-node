15f43e80648d328ff606fe046f621efc3277e308
henrymatt <henrymatt@me.com>
2015-07-10 08:47:26 +0900

    Finishes Deliver functionality of Publishing page

--- public/app/platform/vtmsPlatform.js
+++ public/app/platform/vtmsPlatform.js
@@ -1,5 +1,5 @@
 angular.module('vtms').factory('vtmsPlatform', function($resource) {
-  var PlatformResource = $resource('/api/platform/:id', {id: "@id"}, {
+  var PlatformResource = $resource('/api/platforms/:id', {id: "@id"}, {
     update: {method:'PUT', isArray:false}
   });
   
--- public/app/publishDates/vtmsPublishDateListController.js
+++ public/app/publishDates/vtmsPublishDateListController.js
@@ -1,3 +1,21 @@
-angular.module('vtms').controller('vtmsPublishDateListController', function($scope, vtmsPublishDate, vtmsPlatform) {
+angular.module('vtms').controller('vtmsPublishDateListController', function($scope, vtmsPublishDate, vtmsPlatform, vtmsNotifier) {
   $scope.publishDates = vtmsPublishDate.getIncomplete();
+  
+  $scope.platforms = [
+    {display: "Pod101 Site", tableValue: "pod101"},
+    {display: "YouTube", tableValue: "YouTube"},
+    {display: "Roku", tableValue: "roku"}
+  ];
+  
+  $scope.selectedPlatform = $scope.platforms[0];
+  
+  $scope.deliverPublishDate = function(publishDate) {
+    publishDate.deliver().then(function(newData) {
+      var indexToDelete = $scope.publishDates.indexOf(publishDate);
+      $scope.publishDates.splice(indexToDelete, 1);
+      
+      var lessonString = publishDate.lesson.languageSery.title + " #" + publishDate.lesson.number;
+      vtmsNotifier.notify("Delivered " + lessonString + " for " + publishDate.platform.name + ".");
+    });
+  };
 });
