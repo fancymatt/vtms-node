eed4b1c80569c594d48b7cac35ff276efc93c2b7
henrymatt <henrymatt@me.com>
2015-07-27 21:59:19 +0900

    Converts other locations task list appears to use directive

--- public/app/lessons/vtmsLessonDetailsController.js
+++ public/app/lessons/vtmsLessonDetailsController.js
@@ -5,6 +5,24 @@ angular.module('vtms').controller('vtmsLessonDetailsController', function($scope
   ctrl.taskList = vtmsTask.getList({id: ctrl.lessonId});
   ctrl.issuesList = vtmsIssue.getListForLesson({id: ctrl.lessonId});
   
+  ctrl.tasksConfig = {
+    title: 'Tasks',
+    type: 'default',
+    actions: {
+      activate: true,
+      deactivate: true,
+      complete: true,
+      incomplete: true,
+    },
+    columns: {
+      lesson: false,
+      task: true,
+      teamMember: true,
+      status: true,
+      dueDate: true
+    }
+  }
+  
   ctrl.issuesConfig = {
     actions: {
       complete: true,
