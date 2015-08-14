b491f118f8556eae951b81921b3e7df02f092013
henrymatt <henrymatt@me.com>
2015-08-14 16:02:56 +0900

    Some small tweaks to permissions

--- public/app/account/vtmsUser.js
+++ public/app/account/vtmsUser.js
@@ -7,6 +7,10 @@ angular.module('vtms').factory('vtmsUser', function($resource) {
     return this.role === 'admin';
   }
   
+  UserResource.prototype.isManager = function() {
+    return this.role === 'manager' || this.role === 'admin';
+  }
+  
   UserResource.prototype.isTeamMember = function() {
     return this.fkTeamMember > 0;
   }
