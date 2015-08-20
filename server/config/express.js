a710eaaaa78b8bdfdd39fda3bb3c822907af609a
henrymatt <henrymatt@me.com>
2015-08-20 18:02:34 +0900

    Fixes problems with express config and session store

--- server/config/express.js
+++ server/config/express.js
@@ -1,18 +1,28 @@
 var express = require('express'),
+    sequelize = require('./sequelize.js'),
     logger = require('morgan'),
     bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
     session = require('express-session'),
     passport = require('passport');
 
+var SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 module.exports = function(app, config) {
   app.set('view engine', 'jade');
   app.set('views', config.path + 'server/views/');
   app.use(logger('dev'));
   app.use(cookieParser());
-  app.use(bodyParser.urlencoded());
+  app.use(bodyParser.urlencoded({extended: false}));
   app.use(bodyParser.json());
-  app.use(session({secret: 'matt is so secretive'}));
+  app.use(session({
+    secret: 'matt is so secretive',
+    resave: true,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  }));
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(express.static(config.path + 'public'));
