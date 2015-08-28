b86a7db8f9da04914dc1807bec0519f085e1e992
henrymatt <henrymatt@me.com>
2015-08-28 11:12:51 +0900

    Redirects to main page after log out

--- public/app/account/vtmsNavBarLoginController.js
+++ public/app/account/vtmsNavBarLoginController.js
@@ -1,6 +1,6 @@
 angular.module('vtms').controller('vtmsNavBarLoginController', function($scope, $http, vtmsIdentity, vtmsNotifier, vtmsAuth, $location) {
   $scope.identity = vtmsIdentity;
-  
+
   $scope.signin = function(username, password) {
     vtmsAuth.authenticateUser(username, password).then(function(success) {
       if(success) {
@@ -13,12 +13,13 @@ angular.module('vtms').controller('vtmsNavBarLoginController', function($scope, 
       }
     });
   };
-  
+
   $scope.signout = function() {
     vtmsAuth.logoutUser().then(function() {
       $scope.username = "";
       $scope.password = "";
       vtmsNotifier.notify('You have successfully signed out');
+      $location.path('/');
     });
   };
 });
