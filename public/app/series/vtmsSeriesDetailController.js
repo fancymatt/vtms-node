06988f802b6d729066957200fafc673349505bb5
henrymatt <henrymatt@me.com>
2015-09-01 08:16:23 +0900

    Sorts series by level number

--- public/app/series/vtmsSeriesDetailController.js
+++ public/app/series/vtmsSeriesDetailController.js
@@ -1,11 +1,11 @@
 angular.module('vtms').controller('vtmsSeriesDetailController', function($scope, vtmsSeries, vtmsLanguageSeries, $routeParams) {
   $scope.series = vtmsSeries.get({id: $routeParams.id});
   $scope.languageSeriesList = vtmsLanguageSeries.getList({id: $routeParams.id});
-  
+
   $scope.sortOptions = [
-    {value: "title", text: "Sort by Title"},
-    {value: "language.name", text: "Sort by Language"}
+    {value: ["title", "level.number"], text: "Sort by Title"},
+    {value: ["language.name", "level.number"], text: "Sort by Language"}
   ];
-  
+
   $scope.selectedSortOption = $scope.sortOptions[0].value;
 });
