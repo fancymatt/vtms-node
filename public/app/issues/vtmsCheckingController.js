f32b2912bffa354018f3f264f5cafcf90b1def11
henrymatt <henrymatt@me.com>
2015-09-01 08:38:12 +0900

    Adds buttons to mark lessons as checked from checking page, Fixes #36

--- public/app/issues/vtmsCheckingController.js
+++ public/app/issues/vtmsCheckingController.js
@@ -1,10 +1,10 @@
-angular.module('vtms').controller('vtmsCheckingController', function($scope, vtmsLesson, vtmsIssue, $routeParams, vtmsNotifier, $sce, $q) {
+angular.module('vtms').controller('vtmsCheckingController', function($scope, vtmsLesson, vtmsIssue, $routeParams, vtmsIdentity, vtmsNotifier, $sce, $q) {
   var ctrl = this;
-  
+
   console.log($routeParams);
-  
+
   ctrl.lessonId = $routeParams.id;
-  
+
   ctrl.issuesConfig = {
     title: 'Issues for lesson',
     update: function() {
@@ -27,29 +27,41 @@ angular.module('vtms').controller('vtmsCheckingController', function($scope, vtm
       creator: false
     }
   };
-  
-  
-  
+
+  ctrl.identity = vtmsIdentity.currentUser;
+
   cleanUrl = function(url) {
     var index = url.indexOf('.mp4');
     var cleanedUrl = url.slice(0, index + 4);
     return cleanedUrl + "?dl=1";
   };
-  
+
   vtmsLesson.get({id: ctrl.lessonId}, function(lesson) {
     ctrl.lesson = lesson;
     ctrl.config.sources[0].src = $sce.trustAsResourceUrl(cleanUrl(lesson.qaUrl));
   });
-  
+
   ctrl.config = {
     sources: [
       {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"}
-    ], 
+    ],
     theme: {
       url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
     }
   };
-  
+
+  ctrl.markAsLanguageApproved = function() {
+    ctrl.lesson.markAsLanguageChecked().then(function(newData) {
+      angular.extend(ctrl.lesson, newData);
+    });
+  };
+
+  ctrl.markAsVideoApproved = function() {
+    ctrl.lesson.markAsVideoChecked().then(function(newData) {
+      angular.extend(ctrl.lesson, newData);
+    });
+  };
+
   $scope.$on('time:updated', function(event, time) {
     ctrl.currentTime = time;
   });
