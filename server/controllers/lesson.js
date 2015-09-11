1a666c2226665760c5f23f66f6923592889cc4e6
henrymatt <henrymatt@me.com>
2015-09-11 11:10:50 +0900

    Fixing = => === bug

--- server/controllers/lesson.js
+++ server/controllers/lesson.js
@@ -247,7 +247,7 @@ exports.getReadyToRenderLessons = function (req, res) {
       var renderQueueLessons = [];
       for(var i = 0; i < lessons.length; i++) {
         // Cannot insert value as null, so this catches lessons that were unqueued
-        if(lessons[i].queuedTime = '0000-00-00') lessons[i].queuedTime = null;
+        if(lessons[i].queuedTime === '0000-00-00') lessons[i].queuedTime = null;
         if(lessons[i].lastTaskTime > lessons[i].queuedTime || lessons[i].lastIssueTime > lessons[i].queuedTime || lessons[i].queuedTime === '0000-00-00') {
           if(lessons[i].checkedLanguage) {
             // if it's checked language, then we shouldn't export again until all tasks are completed
