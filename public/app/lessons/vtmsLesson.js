fbd0febdcc43cb5ad4468eb164771a84c235bd2c
henrymatt <henrymatt@me.com>
2015-09-11 11:26:09 +0900

    Changes 0000-00-00 ... to ''

--- public/app/lessons/vtmsLesson.js
+++ public/app/lessons/vtmsLesson.js
@@ -57,7 +57,7 @@ angular.module('vtms').factory('vtmsLesson', function($resource, $q, vtmsNotifie
 
     this.update({
       isQueued: false,
-      queuedTime: '0000-00-00 00:00:00'
+      queuedTime: ''
     }).then(function(newData) {
       vtmsNotifier.notify(notification);
       dfd.resolve(newData);
